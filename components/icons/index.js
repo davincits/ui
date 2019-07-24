696815055cb3e02802cab55edab9f4823266283f
Serhii Melnykov <mlnkv87@gmail.com>
2019-07-24 14:46:25 +0300

    ...

---
+++ components/icons/index.js
@@ -0,0 +1,13 @@
+import Close from './Close';
+import Dollar from './Dollar';
+import Favorite from './Favorite';
+import ThumbUp from './ThumbUp';
+import Verified from './Verified';
+
+export {
+  Close,
+  Dollar,
+  Favorite,
+  ThumbUp,
+  Verified,
+};
