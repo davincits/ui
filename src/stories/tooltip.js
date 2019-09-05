b0fccc725a5c3f627e9417457b50191891a92e8b
Serhii Melnykov <mlnkv87@gmail.com>
2019-09-05 17:08:57 +0300

    DAV-2478

--- src/stories/tooltip.js
+++ src/stories/tooltip.js
@@ -8,9 +8,12 @@ storiesOf('Tooltip', module)
   .add('common', () => (
     <Container>
       {() => (
-        <div style={{ padding: '16px 50px', width: '180px', textAlign: 'center' }}>
+        <div style={{ padding: '50px', textAlign: 'center', display: 'flex' }}>
           <Tooltip tooltip="Lorem ipsum dolor sit amet...">
-            <div style={{ border: '1px solid #dcdcdc', borderRadius: '4px', padding: '8px' }}>Hover me</div>
+            <div style={{ border: '1px solid #f0f0f0', borderRadius: '2px', padding: '4px', margin: '4px' }}>Simple tooltip</div>
+          </Tooltip>
+          <Tooltip tooltip={`Lorem ipsum dolor sit amet, consectetur\nadipisicing elit, sed do eiusmod tempor\nincididunt ut labore et dolore magna aliqua`}>
+            <div style={{ border: '1px solid #f0f0f0', borderRadius: '2px', padding: '4px', margin: '4px' }}>Multiline tooltip</div>
           </Tooltip>
         </div>
       )}
