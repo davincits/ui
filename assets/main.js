b9a530bf64bef18a5bfbf5e70c95423230ad765e
Serhii Melnykov <mlnkv87@gmail.com>
2019-09-12 23:06:37 +0300

    ...

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,11 @@
 {
   const nav = document.querySelector('nav');
   if (nav) {
-    document.querySelectorAll('h2, h3').forEach(el => {
+    document.querySelectorAll('h2, h3').forEach((el, index) => {
       const a = document.createElement('a');
       const name = el.nodeName;
       const text = el.textContent;
-      const id = text.replace(/ /g, '-').replace(/[^\w-]+/g,'');
+      const id = text.replace(/ /g, '-').replace(/[^\w-]+/g,'') + '-' + index;
       a.classList.add(`tag-${name}`);
       a.textContent = text;
       a.href = `#${id}`;
