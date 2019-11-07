34aeb6003da372edd630ed65d0849886e95bda3d
Serhii Melnykov <mlnkv87@gmail.com>
2019-11-07 17:39:36 +0300

    v0.4.0

--- storybook/vendors~main.06227755fa84938eb0e8.bundle.js
+++ storybook/vendors~main.06227755fa84938eb0e8.bundle.js
@@ -69,4 +69,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.98ebd1d1ce196f3de3e5.bundle.js.map
+//# sourceMappingURL=vendors~main.06227755fa84938eb0e8.bundle.js.map
